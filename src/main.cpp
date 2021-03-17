4b769ac78cfa86e291afe86eb31964bc5e30e36a
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-03-17 11:10:55 +0100

    Output LMIC debug infos

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,88 @@ const lmic_pinmap lmic_pins = {
     .dio = {PIN_LMIC_DIO0, PIN_LMIC_DIO1, PIN_LMIC_DIO2},
 };
 
+// opmode def
+// https://github.com/mcci-catena/arduino-lmic/blob/89c28c5888338f8fc851851bb64968f2a493462f/src/lmic/lmic.h#L233
+void LoraWANPrintLMICOpmode(void)
+{
+    Serial.print(F("LMIC.opmode: "));
+    if (LMIC.opmode & OP_NONE){ Serial.print(F("OP_NONE ")); }
+    if (LMIC.opmode & OP_SCAN){ Serial.print(F("OP_SCAN ")); }
+    if (LMIC.opmode & OP_TRACK){ Serial.print(F("OP_TRACK ")); }
+    if (LMIC.opmode & OP_JOINING){ Serial.print(F("OP_JOINING ")); }
+    if (LMIC.opmode & OP_TXDATA){ Serial.print(F("OP_TXDATA ")); }
+    if (LMIC.opmode & OP_POLL){ Serial.print(F("OP_POLL ")); }
+    if (LMIC.opmode & OP_REJOIN){ Serial.print(F("OP_REJOIN ")); }
+    if (LMIC.opmode & OP_SHUTDOWN){ Serial.print(F("OP_SHUTDOWN ")); }
+    if (LMIC.opmode & OP_TXRXPEND){ Serial.print(F("OP_TXRXPEND ")); }
+    if (LMIC.opmode & OP_RNDTX){ Serial.print(F("OP_RNDTX ")); }
+    if (LMIC.opmode & OP_PINGINI){ Serial.print(F("OP_PINGINI ")); }
+    if (LMIC.opmode & OP_PINGABLE){ Serial.print(F("OP_PINGABLE ")); }
+    if (LMIC.opmode & OP_NEXTCHNL){ Serial.print(F("OP_NEXTCHNL ")); }
+    if (LMIC.opmode & OP_LINKDEAD){ Serial.print(F("OP_LINKDEAD ")); }
+    if (LMIC.opmode & OP_LINKDEAD){ Serial.print(F("OP_LINKDEAD ")); }
+    if (LMIC.opmode & OP_TESTMODE){ Serial.print(F("OP_TESTMODE ")); }
+    if (LMIC.opmode & OP_UNJOIN){ Serial.print(F("OP_UNJOIN ")); }
+    Serial.println("");
+}
+
+void LoraWANDebug(lmic_t lmic_check)
+{
+    Serial.println("");
+    Serial.println("");
+    
+    LoraWANPrintLMICOpmode();
+
+    Serial.print(F("LMIC.seqnoUp = "));
+    Serial.println(lmic_check.seqnoUp); 
+
+    Serial.print(F("LMIC.globalDutyRate = "));
+    Serial.print(lmic_check.globalDutyRate);
+    Serial.print(F(" osTicks, "));
+    Serial.print(osticks2ms(lmic_check.globalDutyRate)/1000);
+    Serial.println(F(" sec"));
+
+    Serial.print(F("LMIC.globalDutyAvail = "));
+    Serial.print(lmic_check.globalDutyAvail);
+    Serial.print(F(" osTicks, "));
+    Serial.print(osticks2ms(lmic_check.globalDutyAvail)/1000);
+    Serial.println(F(" sec"));
+
+    Serial.print(F("LMICbandplan_nextTx = "));
+    Serial.print(LMICbandplan_nextTx(os_getTime()));
+    Serial.print(F(" osTicks, "));
+    Serial.print(osticks2ms(LMICbandplan_nextTx(os_getTime()))/1000);
+    Serial.println(F(" sec"));
+
+    Serial.print(F("os_getTime = "));
+    Serial.print(os_getTime());
+    Serial.print(F(" osTicks, "));
+    Serial.print(osticks2ms(os_getTime()) / 1000);
+    Serial.println(F(" sec"));
+
+    Serial.print(F("LMIC.txend = "));
+    Serial.println(lmic_check.txend);
+    Serial.print(F("LMIC.txChnl = "));
+    Serial.println(lmic_check.txChnl);
+
+    Serial.println(F("Band \tavail \t\tavail_sec\tlastchnl \ttxcap"));
+    for (u1_t bi = 0; bi < MAX_BANDS; bi++)
+    {
+        Serial.print(bi);
+        Serial.print("\t");
+        Serial.print(lmic_check.bands[bi].avail);
+        Serial.print("\t\t");
+        Serial.print(osticks2ms(lmic_check.bands[bi].avail)/1000);
+        Serial.print("\t\t");
+        Serial.print(lmic_check.bands[bi].lastchnl);
+        Serial.print("\t\t");
+        Serial.println(lmic_check.bands[bi].txcap);
+        
+    }
+    Serial.println("");
+    Serial.println("");
+}
+
 void PrintRuntime()
 {
     long seconds = millis() / 1000;
@@ -256,6 +338,8 @@ void setup()
     {
         LoadLMICFromRTC();
     }
+
+    LoraWANDebug(LMIC);
 
     // Start job (sending automatically starts OTAA too)
     do_send(&sendjob);
